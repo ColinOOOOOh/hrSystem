62a9b2e589286e35d9c83116c96c59f61c73db33
davis <davis@davis-Aspire-E1-571G>
2015-12-29 22:31:59 -0800

    修复引用库之间的关联问题;并使用spring framework 3.0;

--- test/base/UnitTestBase.java
+++ test/base/UnitTestBase.java
@@ -6,11 +6,11 @@
 
 package base;
 
+import org.apache.commons.lang.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.springframework.beans.BeansException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.util.StringUtils;
 
 /**
  *
